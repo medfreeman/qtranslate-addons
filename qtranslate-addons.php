5d4b8615899f1937d372185f01e6ba76dfadc54b
Mehdi Lahlou <mehdi.lahlou@free.fr>
2014-04-18 02:25:36 +0200

    removed sample readme, added github uri to main php file to allow auto update

--- qtranslate-addons.php
+++ qtranslate-addons.php
@@ -21,7 +21,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: 
+ * GitHub Plugin URI: https://github.com/medfreeman/qtranslate-addons
  */
 
 // If this file is called directly, abort.
